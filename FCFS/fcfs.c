d958351b2ca8d60efad59c97c635f40bb99eea1d
Md Abdul Aziz Zisan <77972913+mdabdulazizzisan@users.noreply.github.com>
2024-11-02 09:51:18 +0600

    Create fcfs.c
    created the file and generated the basic structure.

---
+++ FCFS/fcfs.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void averageTime(int processes[], int burstTime[], int n){
+    return;
+}
+
+int main(void){
+    int processes[] = {1, 2, 3};
+    int burstTime[] = {10, 5, 8};
+    int n = sizeof processes / sizeof processes[0];
+
+
+    //call the necessary functions and run the program
+    averageTime(processes, burstTime, n);
+}
